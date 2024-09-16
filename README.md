395cd58f30a1f94794a79636028d87dcba011a92
Tymur Lysenko <19238280+Sitiritis@users.noreply.github.com>
2024-09-16 13:49:55 -0400

    Setup qmk; make submodules work

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 ### [`qmk_firmware`](https://github.com/qmk/qmk_firmware)
 
 Keychron's fork of [`qmk_firmware`](https://github.com/qmk/qmk_firmware) at the branch `wireless_playground` is checked in to the sources of this repo using `git subtree` under the `keyboard/qmk/firmware` path.
+
+#### Setup
+
+##### Pull `qmk_firmware` submodules
+
+```shell
+git submodule update --init --recursive
+```
+
+> [!NOTE]
+>
+> The `.gitmodules` file just duplicates the contents of the `.gitmodules` files from the dependencies checked in with `git subtree`. The repo maintainer added the corresponding `git subtree` prefix to the relevant places in the repo root `.gitmodules`.
+
+##### QMK CLI
+
+Run the following QMK CLI command from the `qmk_firmware` repo root:
+
+```shell
+cd keyboard/qmk/firmware
+qmk setup
+```
+
+#### Useful links for `qmk_firmware`
+
+Instructions for [setting up the firmware build](https://docs.qmk.fm/newbs_getting_started) can be found in the official docs.
--- keyboard/qmk/firmware/lib/python/qmk/submodules.py
+++ keyboard/qmk/firmware/lib/python/qmk/submodules.py
@@ -2,6 +2,17 @@
 """
 from milc import cli
 
+# This is a custom workaround to fix paths since qmk_firmware is checked in with git subtree.
+# The function will remove the subtree prefix from a given path.
+# It is used to run the submodule commands as if they were run from the qmk_firmware repo root.
+# With these modifications it is assumed that the script is executed from the qmk firmware root directory.
+def remove_subtree_prefix(
+    path,
+    # TODO: configure prefix here (should be the same one as used by the subtree command).
+    # Ideally, this should come from cli args.
+    prefix='keyboard/qmk/firmware/',
+):
+    return path.replace(prefix, '', 1)
 
 def status():
     """Returns a dictionary of submodules.
@@ -24,37 +35,39 @@ def status():
     gitmodule_config = cli.run(['git', 'config', '-f', '.gitmodules', '-l'], timeout=30)
     for line in gitmodule_config.stdout.splitlines():
         key, value = line.split('=', maxsplit=2)
+        sanitized_submodule = remove_subtree_prefix(value)
         if key.endswith('.path'):
-            submodules[value] = {'name': value, 'status': None}
+            submodules[sanitized_submodule] = {'name': sanitized_submodule, 'status': None}
 
     git_cmd = cli.run(['git', 'submodule', 'status'], timeout=30)
     for line in git_cmd.stdout.splitlines():
         status = line[0]
         githash, submodule = line[1:].split()[:2]
-        submodules[submodule]['githash'] = githash
+        sanitized_submodule = remove_subtree_prefix(submodule)
+        submodules[sanitized_submodule]['githash'] = githash
 
         if status == '-':
-            submodules[submodule]['status'] = None
+            submodules[sanitized_submodule]['status'] = None
         elif status == '+':
-            submodules[submodule]['status'] = False
+            submodules[sanitized_submodule]['status'] = False
         elif status == ' ':
-            submodules[submodule]['status'] = True
+            submodules[sanitized_submodule]['status'] = True
         else:
             raise ValueError('Unknown `git submodule status` sha-1 prefix character: "%s"' % status)
 
     submodule_logs = cli.run(['git', 'submodule', '-q', 'foreach', 'git --no-pager log --no-show-signature --pretty=format:"$sm_path%x01%h%x01%ad%x01%s%x0A" --date=iso -n1'])
     for log_line in submodule_logs.stdout.splitlines():
         r = log_line.split('\x01')
-        submodule = r[0]
-        submodules[submodule]['shorthash'] = r[1] if len(r) > 1 else ''
-        submodules[submodule]['last_log_timestamp'] = r[2] if len(r) > 2 else ''
-        submodules[submodule]['last_log_message'] = r[3] if len(r) > 3 else ''
+        sanitized_submodule = remove_subtree_prefix(r[0])
+        submodules[sanitized_submodule]['shorthash'] = r[1] if len(r) > 1 else ''
+        submodules[sanitized_submodule]['last_log_timestamp'] = r[2] if len(r) > 2 else ''
+        submodules[sanitized_submodule]['last_log_message'] = r[3] if len(r) > 3 else ''
 
     submodule_tags = cli.run(['git', 'submodule', '-q', 'foreach', '\'echo $sm_path `git describe --tags`\''])
     for log_line in submodule_tags.stdout.splitlines():
         r = log_line.split()
-        submodule = r[0]
-        submodules[submodule]['describe'] = r[1] if len(r) > 1 else ''
+        sanitized_submodule = remove_subtree_prefix(r[0])
+        submodules[sanitized_submodule]['describe'] = r[1] if len(r) > 1 else ''
 
     return submodules
 
