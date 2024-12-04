0a4a610396b8cb3876caaa8e2dd3f96e42430721
Tim L <19238280+tymur-l@users.noreply.github.com>
2024-12-03 23:25:55 -0500

    Add neovim config

---
+++ dotfiles/neovim/setup-config.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# https://specifications.freedesktop.org/basedir-spec/latest/
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+
+NVIM_CONFIG_DIR="${XDG_CONFIG_HOME}/nvim"
+
+commands_to_run=(
+  "rm -rf ${NVIM_CONFIG_DIR}"
+  "ln -s ${SCRIPT_DIR}/config/ ${NVIM_CONFIG_DIR}"
+)
+
+echo "The script will replace your neovim config with the config from this repo:"
+for command in "${commands_to_run[@]}"; do
+  echo "\$ ${command}"
+done
+
+echo "Run the commands?"
+select yn in "yes" "no"; do
+  case "${yn}" in
+    'yes' )
+      for command in "${commands_to_run[@]}"; do
+        eval "${command}"
+      done
+      break;;
+
+    * ) break;;
+
+  esac
+done
