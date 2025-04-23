02fe438920f77e6c436e96084a873866542a2a00
Tim L <19238280+tymur-l@users.noreply.github.com>
2025-04-22 20:42:46 -0400

    Add keymapping to jump to context

--- dotfiles/neovim/config/init.lua
+++ dotfiles/neovim/config/init.lua
@@ -866,24 +866,48 @@ local nvim_treesitter_spec = bpu:declare_lazy_spec(
 local nvim_treesitter_context_spec = bpu:declare_lazy_spec(
   "config.infra.plugins.nvim-treesitter-context",
   {
-    opts = {
-      -- https://github.com/nvim-treesitter/nvim-treesitter-context?tab=readme-ov-file#configuration
-      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-      multiwindow = true, -- Enable multiwindow support.
-      max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
-      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-      line_numbers = true,
-      multiline_threshold = 20, -- Maximum number of lines to show for a single context
-      trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
-      mode = "cursor", -- Line used to calculate context. Choices: "cursor", "topline"
-      -- Separator between context and content. Should be a single character string, like "-".
-      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-      separator = nil,
-      zindex = 20, -- The Z-index of the context window
-      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-    }
+    config = function()
+      local nvim_treesitter_context = require("treesitter-context")
+
+      nvim_treesitter_context.setup({
+        -- https://github.com/nvim-treesitter/nvim-treesitter-context?tab=readme-ov-file#configuration
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        multiwindow = true, -- Enable multiwindow support.
+        max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
+        mode = "cursor", -- Line used to calculate context. Choices: "cursor", "topline"
+        -- Separator between context and content. Should be a single character string, like "-".
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+
+      -- Keymappings
+      vim.keymap.set(
+        "n",
+        "<leader><leader>gc",
+        function() nvim_treesitter_context.go_to_context(vim.v.count1) end,
+        { silent = true }
+      )
+    end
   }
 )
+
+--- telescope
+-- local telescope_spec = bpu:declare_lazy_spec(
+--   -- TODO
+--   "config.infra.plugins.telescope",
+--   {
+--     -- TODO: link to config
+--     opts = {
+--       -- TODO
+--     },
+--   }
+-- )
 
 --- Initialize lazy.nvim plugin manager
 local lazy_plugin_infra = require("config.infra.lazy")
