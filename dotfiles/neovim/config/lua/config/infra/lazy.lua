b726a2e974e3f28c295526bc203ec7b95943984b
Tim L <19238280+tymur-l@users.noreply.github.com>
2025-05-04 15:15:50 -0400

    Setup isolated testing for neovim config

--- dotfiles/neovim/config/lua/config/infra/lazy.lua
+++ dotfiles/neovim/config/lua/config/infra/lazy.lua
@@ -1,31 +1,19 @@
 local M = {}
 
--- Download lazy.nvim if it has not been downloaded yet
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not(vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  local out = ""
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+local lazy = nil
 
-local lazy = require("lazy")
+local _set_mt = {
+  __metatable = "BkbLazyMt"
+}
 
--- Assume that the mapleader and maplocalleader have already been set by the
--- loading script
+_set_mt.__index = M
 
--- Function that setup lazy.nvim with the plugin configuration managed by the
--- caller
-function M.setup_plugins(setup_config)
+setmetatable(M, _set_mt)
+
+-- Function that setup lazy.nvim with the plugin configuration managed by the caller
+function M.setup_plugins(setup_config, lazy_instance)
+  local lazy_instance = lazy_instance or lazy or error('Lazy is not initialized. Make sure to call .instance() function first or pass require("lazy") as the last argument to this function explicitly.')
+
   -- Caller of the function has control to override the default spec declared here
   local final_config = vim.tbl_deep_extend(
     "force",
@@ -35,7 +23,26 @@ function M.setup_plugins(setup_config)
     setup_config
   )
 
-  lazy.setup(final_config)
+  lazy_instance.setup(final_config)
+end
+
+function M.init()
+  if not lazy then
+    -- Download lazy.nvim if it has not been downloaded yet
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not(vim.uv or vim.loop).fs_stat(lazypath) then
+      local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+      local out = vim.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+      if vim.v.shell_error ~= 0 then
+        error("Failed to clone lazy.nvim:\n" .. "ErrorMsg: " .. out)
+      end
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    lazy = require("lazy")
+
+    -- Assume that the mapleader and maplocalleader have already been set by the loading script
+  end
 end
 
 return M
--- dotfiles/neovim/tests/minimal_init.lua
+++ dotfiles/neovim/tests/minimal_init.lua
@@ -1,13 +1,21 @@
-#!/usr/bin/env -S nvim --headless -c ':lua test_bkb_all()' -u
+#!/usr/bin/env -S nvim --headless -c 'lua test_bkb_all()' -u
 
 -- Note, users can also run this file interactively using the following command:
 -- nvim -u './tests/minimal_init.lua'
 
+uv = vim.loop
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.rtp:append("./")
-vim.opt.rtp:append("../config")
+local THIS_SCRIPT_ABSOLUTE_PATH_STR = uv.fs_realpath(debug.getinfo(1, "S").source:sub(2))
+local THIS_SCRIPT_DIR_ABSOLUTE_PATH_STR = vim.fn.fnamemodify(THIS_SCRIPT_ABSOLUTE_PATH_STR, ":p:h")
+local CONFIG_DIR_PATH_STR = uv.fs_realpath(THIS_SCRIPT_DIR_ABSOLUTE_PATH_STR .. "/../config")
+
+vim.opt.rtp:append(THIS_SCRIPT_DIR_ABSOLUTE_PATH_STR)
+vim.opt.rtp:append(CONFIG_DIR_PATH_STR)
+
+vim.env.LAZY_STDPATH = THIS_SCRIPT_DIR_ABSOLUTE_PATH_STR .. "/.tests"
 
 local bpu = require("config.infra.utils")
 
@@ -18,35 +26,46 @@ local plenary_lazy_spec = bpu:declare_lazy_spec(
   {}
 )
 
+local lazy_bootstrap = require("bootstrap_lazy")
+local lazy_for_tests = require("lazy")
 local lazy_plugin_infra = require("config.infra.lazy")
-lazy_plugin_infra.setup_plugins({
-  -- All options: https://lazy.folke.io/configuration
-  spec = bpu.final_lazy_spec,
-  install = {
-    missing = true,
+lazy_plugin_infra.setup_plugins(
+  {
+    -- All options: https://lazy.folke.io/configuration
+    spec = bpu.final_lazy_spec,
+    install = {
+      missing = true,
+    },
+    rocks = {
+      enabled = true,
+    },
+    checker = {
+      -- Disable checking for autoupdates, because this is a test runner that is primarily intended for a non-interactive use
+      enabled = false,
+    },
   },
-  rocks = {
-    enabled = true,
-  },
-  checker = {
-    -- Disable checking for autoupdates, because this is a test runner that is primarily intended for a non-interactive use
-    enabled = false,
-  },
-})
+  lazy_for_tests
+)
+
+-- After lazy is setup it may override rtp, so modifying rtp again
+vim.opt.rtp:append(THIS_SCRIPT_DIR_ABSOLUTE_PATH_STR)
+vim.opt.rtp:append(CONFIG_DIR_PATH_STR)
 
 local Path = require("plenary.path")
-local THIS_SCRIPT_ABSOLUTE_PATH_STR = Path.new(debug.getinfo(1, "S").source:sub(2)):absolute()
-local CONFIG_DIR_PATH_STR = Path.new(THIS_SCRIPT_ABSOLUTE_PATH_STR):parent():joinpath("../config"):absolute()
 
 function test_bkb_all()
   local plenary_test_harness = require("plenary.test_harness")
 
   local test_dirs_glob = tostring(Path.new(CONFIG_DIR_PATH_STR):joinpath("**/tests/"))
-  local test_directories = vim.split(vim.fn.glob(test_dirs_glob), "\n", { trimempty = true })
+  local test_directories = vim.split(
+    vim.fn.glob(test_dirs_glob),
+    "\n",
+    { trimempty = true }
+  )
 
   for _, test_dir in pairs(test_directories) do
     local plenary_test_args = test_dir .. " " .. vim.inspect({
-      init = THIS_SCRIPT_ABSOLUTE_PATH_STR
+      init = THIS_SCRIPT_ABSOLUTE_PATH_STR,
     }):gsub("\n", " ")
 
     plenary_test_harness.test_directory_command(plenary_test_args)
